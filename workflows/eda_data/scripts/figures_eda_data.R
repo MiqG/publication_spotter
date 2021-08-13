68b191a02f39b7273666a2731b7ae630fd2566d6
MiqG <miquelangladagirotto@gmail.com>
2021-08-13 19:36:21 +0200

    created workflow EDA data inputs

---
+++ workflows/eda_data/scripts/figures_eda_data.R
@@ -0,0 +1,14 @@
+#
+# Author: Miquel Anglada Girotto
+# Contact: miquel [dot] anglada [at] crg [dot] eu
+#
+# Script purpose
+# --------------
+# EDA of datasets used.
+# 
+# Outline
+# -------
+# - gene dependencies across cell lines
+# - mRNA levels across cell lines
+# - imputed PSI across cell lines
+# - consider transcriptome indices: stemness
