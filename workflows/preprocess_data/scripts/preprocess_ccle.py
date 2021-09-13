f2f039e0ca75841e688eb6bcb52453deee882519
MiqG <miquelangladagirotto@gmail.com>
2021-09-13 14:57:35 +0200

    refactored preprocessing CCLE data matrices

--- workflows/preprocess_data/scripts/preprocess_ccle.py
+++ workflows/preprocess_data/scripts/preprocess_ccle.py
@@ -14,10 +14,11 @@
 
 import argparse
 import pandas as pd
+import numpy as np
 import string
 
 # variables
-SAVE_PARAMS = {'sep':'\t', 'index':False}
+SAVE_PARAMS = {'sep':'\t', 'index':False, 'compression':'gzip'}
 THRESH_NON_MISSING = 50
 
 """
@@ -26,74 +27,140 @@ Development
 import os
 ROOT = '/home/miquel/projects/publication_splicing_dependency'
 RAW_DIR = os.path.join(ROOT,'data','raw')
-raw_psi_file = os.path.join(RAW_DIR,'inhouse','CCLE','CCLE_ALL_INCLUSION_LEVELS_FULL-Hs2935-minN_1-minSD_0-noVLOW-min_ALT_use25-Tidy.tab')
-raw_metadata_file = os.path.join(ROOT,'data','raw','DepMap','achilles_ccle','sample_info.csv')
-ccle_cancertypes_file = os.path.join(ROOT,'data','raw','articles','Yu2019','ccle_metadata.xls')
+PREP_DIR = os.path.join(ROOT,'data','prep')
+
+# metadata
+dataset = 'metadata'
+sample_info_file = os.path.join(RAW_DIR,'DepMap','achilles_ccle','sample_info.csv')
+ccle_cancer_types_file = os.path.join(RAW_DIR,'articles','Yu2019','ccle_metadata.xls')
+sample_annot_file = os.path.join(RAW_DIR,'CCLE','ENA_filereport-PRJNA523380-CCLE.tsv')
+mat_file, metadata_file, event_type = '', '', ''
+
+# psi
+dataset = 'event_psi'
+event_type = 'ALTD'
+mat_file = os.path.join(RAW_DIR,'CCLE','vast_out','PSI-minN_1-minSD_0-noVLOW-min_ALT_use25-Tidy.tab.gz')
+metadata_file = os.path.join(PREP_DIR,'metadata','CCLE.tsv.gz')
+sample_info_file, ccle_cancer_types_file, sample_annot_file = '','',''
+
+# genexpr
+dataset = 'genexpr_tpm'
+mat_file = os.path.join(RAW_DIR,'CCLE','vast_out','TPM-hg38-1019.tab.gz')
+metadata_file = os.path.join(PREP_DIR,'metadata','CCLE.tsv.gz')
+sample_info_file, ccle_cancer_types_file, sample_annot_file, event_type = '','','',''
 """
 
+
 ##### FUNCTIONS #####
-def load_data(psi_file, metadata_file, ccle_cancertypes_file):
-    psi = pd.read_table(psi_file, index_col=0)
-    metadata = pd.read_csv(metadata_file)
-    cancertypes = pd.read_excel(ccle_cancertypes_file)
-
-    return psi, metadata, cancertypes
-
-
-def format_cell_names(x):
-    x = pd.Series(x)
-    x = (
-        x.str.replace("^.+?\\.", "")
-        .str.replace("\\.+?.", "")
-        .str.replace(rf'[{string.punctuation}]', '')
-        .str.upper()
-    )
-
-    return x.values
-
-
-def preprocess_ccle(psi, metadata, cancertypes, event_type):
-    # add cancertypes to metadata
-    metadata = pd.merge(
-        metadata,
-        cancertypes.rename(
-            columns={"CCLE_name": "CCLE_Name", "disease": "cancer_type"}
-        ),
-        on="CCLE_Name",
-        how='left'
-    )
-
-    # drop rows missing all values
-    is_na = psi.isnull()
-    non_missing = is_na.shape[1] - is_na.sum(1)
-    to_keep = non_missing >= THRESH_NON_MISSING
-    psi = psi.loc[to_keep].copy()
-
-    # rename psi columns
-    psi.columns = format_cell_names(psi.columns)
+def load_data(
+        dataset, 
+        sample_info_file, ccle_cancer_types_file, sample_annot_file,
+        metadata_file, mat_file, event_type
+    ):
     
-    # subset
-    common_samples = set(psi.columns).intersection(metadata['stripped_cell_line_name'])
-    psi = psi[common_samples]
-    metadata = metadata.loc[metadata['stripped_cell_line_name'].isin(common_samples)]
-    
-    # rename
-    psi = psi.rename(columns = metadata.set_index('stripped_cell_line_name')['DepMap_ID'].to_dict())
-    
-    # subset PSI by event_type
-    psi = psi.loc[psi.index.str.contains(event_type)].copy()
+    if dataset=='metadata':
+        data = {
+            'sample_info': pd.read_csv(sample_info_file),
+            'cancer_types': pd.read_excel(ccle_cancer_types_file),
+            'sample_annot': pd.read_table(sample_annot_file),
+        }
         
-    return psi, metadata
+    elif dataset=='event_psi':
+        data = {
+            'metadata': pd.read_table(metadata_file)
+        }
+        psi = pd.read_table(mat_file, index_col=0)
+        psi = psi.loc[psi.index.str.contains(event_type)].copy()
+        data['psi'] = psi
+        
+    elif dataset=='genexpr_tpm':
+        data = {
+            'metadata': pd.read_table(metadata_file),
+            'genexpr': pd.read_table(mat_file, index_col=[0,1])
+        }
+        
+    else:
+        print('Invalid dataset.')
+
+    return data
+
+
+def preprocess_ccle(data, dataset):
+    if dataset=='metadata':
+        sample_info = data['sample_info']
+        sample_annot = data['sample_annot'].rename(
+                columns={"sample_alias": "CCLE_Name"}
+            )
+        sample_annot = sample_annot.loc[sample_annot['library_strategy']=='RNA-Seq']
+        cancer_types = data['cancer_types'].rename(
+                columns={"CCLE_name": "CCLE_Name", "disease": "cancer_type"}
+            )
+        
+        # combine
+        metadata = pd.merge(sample_info, cancer_types, on='CCLE_Name', how='left')
+        metadata = pd.merge(metadata, sample_annot[['run_accession','CCLE_Name']], on='CCLE_Name', how='left')
+        
+        output = metadata
+        
+    elif dataset=='event_psi':
+        psi = data['psi']
+        metadata = data['metadata']
+        
+        # drop rows missing all values
+        is_na = psi.isnull()
+        non_missing = is_na.shape[1] - is_na.sum(1)
+        to_keep = non_missing >= THRESH_NON_MISSING
+        psi = psi.loc[to_keep].copy()
+        
+        # remove suffix from vast-tools
+        psi.columns = [c.replace('_1','') for c in psi.columns]
+        
+        # subset
+        common_samples = set(psi.columns).intersection(metadata['run_accession'])
+        psi = psi[common_samples]
+        metadata = metadata.loc[metadata['run_accession'].isin(common_samples)]
+
+        # rename
+        psi = psi.rename(columns = metadata.set_index('run_accession')['DepMap_ID'].to_dict())
+        
+        output = psi.reset_index()
+    
+    elif dataset=='genexpr_tpm':
+        genexpr = data['genexpr']
+        metadata = data['metadata']
+        
+        # log-transform
+        genexpr = np.log2(genexpr + 1)
+        
+        # remove suffix from vast-tools
+        genexpr.columns = [c.replace('_1','') for c in genexpr.columns]
+        
+        # subset
+        common_samples = set(genexpr.columns).intersection(metadata['run_accession'])
+        genexpr = genexpr[common_samples]
+        metadata = metadata.loc[metadata['run_accession'].isin(common_samples)]
+
+        # rename
+        genexpr = genexpr.rename(columns = metadata.set_index('run_accession')['DepMap_ID'].to_dict())
+        
+        output = genexpr.reset_index().drop(columns='ID')
+        
+    return output
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--raw_psi_file", type=str)
-    parser.add_argument("--raw_metadata_file", type=str)
-    parser.add_argument("--ccle_cancertypes_file", type=str)
-    parser.add_argument("--prep_psi_file", type=str)
-    parser.add_argument("--prep_metadata_file", type=str)
-    parser.add_argument("--event_type", type=str)
+    parser.add_argument("--dataset", type=str, default="")
+    # metadata
+    parser.add_argument("--sample_info_file", type=str, default="")
+    parser.add_argument("--ccle_cancer_types_file", type=str, default="")
+    parser.add_argument("--sample_annot_file", type=str, default="")
+    # PSI or genexpr matrices
+    parser.add_argument("--metadata_file", type=str, default="")
+    parser.add_argument("--event_type", type=str, default="")
+    parser.add_argument("--mat_file", type=str, default="")
+    # generic
+    parser.add_argument("--output_file", type=str)
 
     args = parser.parse_args()
 
@@ -102,20 +169,26 @@ def parse_args():
 
 def main():
     args = parse_args()
-    raw_psi_file = args.raw_psi_file
-    raw_metadata_file = args.raw_metadata_file
-    ccle_cancertypes_file = args.ccle_cancertypes_file
-    prep_psi_file = args.prep_psi_file
-    prep_metadata_file = args.prep_metadata_file
+    dataset = args.dataset
+    sample_info_file = args.sample_info_file
+    ccle_cancer_types_file = args.ccle_cancer_types_file
+    sample_annot_file = args.sample_annot_file
+    metadata_file = args.metadata_file
     event_type = args.event_type
+    mat_file = args.mat_file
+    output_file = args.output_file
 
     # load
-    psi, metadata, cancertypes = load_data(raw_psi_file, raw_metadata_file, ccle_cancertypes_file)
-    psi, metadata = preprocess_ccle(psi, metadata, cancertypes, event_type)
+    data = load_data(
+        dataset, 
+        sample_info_file, ccle_cancer_types_file, sample_annot_file,
+        metadata_file, mat_file, event_type
+    )
+    
+    output = preprocess_ccle(data, dataset)
     
     # save
-    psi.reset_index().to_csv(prep_psi_file, compression='gzip', **SAVE_PARAMS)
-    metadata.to_csv(prep_metadata_file, **SAVE_PARAMS)
+    output.to_csv(output_file, **SAVE_PARAMS)
     
 
 ##### SCRIPT #####
