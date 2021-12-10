fixed mutation frequency analysis by event to compute the fold change between the mutation frequency per kilobase in each exon and the average mutation frequency per kilobase of all exons of the gene

workflows/preprocess_data/scripts/count_mutations_per_event_and_variant.py
@@ -6,6 +6,8 @@
 # Script purpose
 # --------------
 # Count number of mutations that mapped to every event from a column of a table.
+# Given its length and the total number of mutations that hit the gene,
+# is an exon more/less mutated than expected?
 
 
 from sso_targets import config, util
@@ -20,7 +22,8 @@ import os
 ROOT = '~/projects/publication_splicing_dependency'
 RAW_DIR = os.path.join(ROOT,'data','raw')
 PREP_DIR = os.path.join(ROOT,'data','prep')
-annotation_file = os.path.join(RAW_DIR,'VastDB','EVENT_INFO-hg38_noseqs.tsv')
+annot_events_file = os.path.join(RAW_DIR,'VastDB','EVENT_INFO-hg38_noseqs.tsv')
+annot_genes_file = os.path.join(RAW_DIR,'ENSEMBL','gene_annotation-hg19.tsv.gz')
 snv_event_file = os.path.join(PREP_DIR,'event_snv','CCLE-EX.tsv.gz')
 snv_gene_file = os.path.join(RAW_DIR,'DepMap','achilles_ccle','CCLE_mutations.csv')
 id_col = 'DepMap_ID'
@@ -40,12 +43,13 @@ def read_snv(snv_file):
     return snv
 
 
-def load_data(snv_event_file, snv_gene_file, annotation_file):
+def load_data(snv_event_file, snv_gene_file, annot_events_file, annot_genes_file):
     snv_event = read_snv(snv_event_file)
     snv_gene = read_snv(snv_gene_file)
-    annot = pd.read_table(annotation_file)
+    annot_events = pd.read_table(annot_events_file)
+    annot_genes = pd.read_table(annot_genes_file)
 
-    return snv_event, snv_gene, annot
+    return snv_event, snv_gene, annot_events, annot_genes
 
 
 def count_mutations_per_item_and_effect(snv, id_col, item_col, effect_col):
@@ -70,10 +74,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--snv_event_file", type=str)
-    parser.add_argument("--snv_gene_file", type=str)
-    parser.add_argument("--annotation_file", type=str)
+    parser.add_argument("--annot_events_file", type=str)
     parser.add_argument("--id_col", type=str)
-    parser.add_argument("--gene_col", type=str)
     parser.add_argument("--event_col", type=str)
     parser.add_argument("--effect_col", type=str)
     parser.add_argument("--output_file", type=str)
@@ -87,36 +89,44 @@ def main():
     args = parse_args()
     snv_event_file = args.snv_event_file
     snv_gene_file = args.snv_gene_file
-    annotation_file = args.annotation_file
+    annot_events_file = args.annot_events_file
+    annot_genes_file = args.annot_genes_file
     output_file = args.output_file
     id_col = args.id_col
-    gene_col = args.gene_col
     event_col = args.event_col
     effect_col = args.effect_col
 
     # load data
-    snv_event, snv_gene, annot = load_data(
-        snv_event_file, snv_gene_file, annotation_file
-    )
+    snv_event, annot_events = load_data(snv_event_file, annot_events_file)
 
     # count mapped mutations
     mut_freq_event = count_mutations_per_item_and_effect(
         snv_event, id_col, event_col, effect_col
     )
-    mut_freq_gene = count_mutations_per_item_and_effect(
-        snv_gene, id_col, gene_col, effect_col
-    )
 
     # for each variant, how many mutations in the gene mapped on the exon?
     mut_freq_event = pd.merge(
-        mut_freq_event, annot[["EVENT", "GENE"]], on="EVENT", how="left"
+        mut_freq_event, annot_events[["EVENT", "GENE", "LE_o"]], on="EVENT", how="left"
     )
-    mut_freq_gene = mut_freq_gene.rename(columns={gene_col: "GENE", "n": "total"})
+    mut_freq_event["event_mut_freq_per_kb"] = (
+        mut_freq_event["n"] / mut_freq_event["LE_o"]
+    ) * 1000
 
-    mut_freq_event = pd.merge(
-        mut_freq_event, mut_freq_gene, on=[effect_col, "GENE"], how="left"
+    # how different is the mutation frequency observed from the average?
+    avg_mut_freq = (
+        mut_freq_event.groupby(["GENE", "Variant_Classification"])[
+            "event_mut_freq_per_kb"
+        ]
+        .mean()
+        .rename("avg_mut_freq")
+        .reset_index()
     )
-    mut_freq_event["ratio"] = mut_freq_event["n"] / mut_freq_event["total"]
+    mut_freq_event = pd.merge(
+        mut_freq_event, avg_mut_freq, how="left", on=["GENE", "Variant_Classification"]
+    )
+    mut_freq_event["fc_mut_freq"] = np.log2(
+        mut_freq_event["event_mut_freq_per_kb"] / mut_freq_event["avg_mut_freq"]
+    )
 
     # save
     mut_freq_event.to_csv(output_file, sep="\t", index=False, compression="gzip")

workflows/preprocess_data/scripts/count_mutations_per_gene_and_variant.py
@@ -4,7 +4,7 @@
 #
 # Script purpose
 # --------------
-# Count mutations per sample by effect. 
+# Count mutations per sample by effect.
 #
 
 import pandas as pd
@@ -27,13 +27,13 @@ effect_col = 'Variant_Classification'
 
 ##### FUNCTIONS #####
 def load_data(snv_file, annotation_file):
-    if snv_file.endswith('.csv'):
+    if snv_file.endswith(".csv"):
         snv = pd.read_csv(snv_file, low_memory=False)
     else:
         snv = pd.read_table(snv_file, low_memory=False)
-        
+
     annot = pd.read_table(annotation_file)
-    
+
     return snv, annot
 
 
@@ -43,65 +43,79 @@ def count_mutations_per_gene_and_effect(snv, id_col, gene_col, effect_col):
     if a sample has multiple mutations in 
     the same gene it is counted only once
     """
-    counts = snv[[id_col, gene_col, effect_col]]\
-                .drop_duplicates()\
-                .groupby([gene_col, effect_col])\
-                .size()\
-                .reset_index()\
-                .rename(columns={0:'n'})
-    
+    counts = (
+        snv[[id_col, gene_col, effect_col]]
+        .drop_duplicates()
+        .groupby([gene_col, effect_col])
+        .size()
+        .reset_index()
+        .rename(columns={0: "n"})
+    )
+
     return counts
 
 
 def compute_mutation_entropy(df_gene_effect):
-    positions = df_gene_effect['Start_position']
-    start, end = int(df_gene_effect['Start'].values[0]), int(df_gene_effect['End'].values[0])
-    
+    positions = df_gene_effect["Start_position"]
+    start, end = (
+        int(df_gene_effect["Start"].values[0]),
+        int(df_gene_effect["End"].values[0]),
+    )
+
     mut_positions, mut_counts = np.unique(positions, return_counts=True)
-    mut_freq = pd.Series(index=np.arange(start, end+1), dtype=np.int)
+    mut_freq = pd.Series(index=np.arange(start, end + 1), dtype=np.int)
     mut_freq[mut_positions] = mut_counts
     mut_freq = mut_freq.fillna(0)
     mut_freq = mut_freq.reset_index()
-    mut_freq.columns = ['position','count']
-    mut_freq['p'] = mut_freq['count'] / len(mut_freq) # probability
-    mut_freq['partial_entropy'] = mut_freq['p']*np.log2(mut_freq['p']) # in bits
-    entropy = -mut_freq['partial_entropy'].sum()
-    
-    return pd.Series([entropy, len(mut_freq)], index=['entropy','length'])
+    mut_freq.columns = ["position", "count"]
+    mut_freq["p"] = mut_freq["count"] / len(mut_freq)  # probability
+    mut_freq["partial_entropy"] = mut_freq["p"] * np.log2(mut_freq["p"])  # in bits
+    entropy = -mut_freq["partial_entropy"].sum()
+
+    return pd.Series([entropy, len(mut_freq)], index=["entropy", "length"])
 
 
 def in_interval(pos, start, end):
-    return (start<=pos) & (pos<=end)
-    
+    return (start <= pos) & (pos <= end)
+
 
 def compute_mutation_entropy_per_gene_and_effect(snv, annot, gene_col, effect_col):
-    cols_oi = ['Start','End','length']
-    df = pd.merge(snv, annot[['Gene']+cols_oi].rename(columns={'Gene':gene_col}), how='left', on='Hugo_Symbol')
-    df = df[[gene_col, effect_col, 'Start_position', 'End_position']+cols_oi].dropna()
-    df['in_interval'] = in_interval(df['Start_position'], df['Start'], df['End'])
-    df = df.loc[df['in_interval']]
-    
-    entropies = df.groupby([gene_col, effect_col])\
-                  .apply(compute_mutation_entropy)\
-                  .reset_index()
-    entropies['min_entropy'] = -(1/entropies['length'])*np.log2(1/entropies['length'])
-    entropies['max_entropy'] = -np.log2(1/entropies['length']) # -len*(1/len)*log2(1/len)
-    entropies['max_rel_entropy'] = entropies['entropy'] / entropies['max_entropy']
-    entropies['min_rel_entropy'] = entropies['entropy'] / entropies['min_entropy']
-    
+    cols_oi = ["Start", "End", "length"]
+    df = pd.merge(
+        snv,
+        annot[["Gene"] + cols_oi].rename(columns={"Gene": gene_col}),
+        how="left",
+        on="Hugo_Symbol",
+    )
+    df = df[[gene_col, effect_col, "Start_position", "End_position"] + cols_oi].dropna()
+    df["in_interval"] = in_interval(df["Start_position"], df["Start"], df["End"])
+    df = df.loc[df["in_interval"]]
+
+    entropies = (
+        df.groupby([gene_col, effect_col]).apply(compute_mutation_entropy).reset_index()
+    )
+    entropies["min_entropy"] = -(1 / entropies["length"]) * np.log2(
+        1 / entropies["length"]
+    )
+    entropies["max_entropy"] = -np.log2(
+        1 / entropies["length"]
+    )  # -len*(1/len)*log2(1/len)
+    entropies["max_rel_entropy"] = entropies["entropy"] / entropies["max_entropy"]
+    entropies["min_rel_entropy"] = entropies["entropy"] / entropies["min_entropy"]
+
     return entropies
-        
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    
-    parser.add_argument('--snv_file',type=str)
-    parser.add_argument('--annotation_file',type=str)
-    parser.add_argument('--id_col',type=str)
-    parser.add_argument('--gene_col',type=str)
-    parser.add_argument('--effect_col',type=str)
-    parser.add_argument('--output_file',type=str)
-    
+
+    parser.add_argument("--snv_file", type=str)
+    parser.add_argument("--annotation_file", type=str)
+    parser.add_argument("--id_col", type=str)
+    parser.add_argument("--gene_col", type=str)
+    parser.add_argument("--effect_col", type=str)
+    parser.add_argument("--output_file", type=str)
+
     args = parser.parse_args()
     return args
 
@@ -109,30 +123,31 @@ def parse_args():
 def main():
     # parse arguments
     args = parse_args()
-    
+
     snv_file = args.snv_file
     annotation_file = args.annotation_file
     id_col = args.id_col
     gene_col = args.gene_col
     effect_col = args.effect_col
     output_file = args.output_file
-    
+
     # load
-    print('Loading data...')
+    print("Loading data...")
     snv, annot = load_data(snv_file, annotation_file)
-    print('Counting mutations per gene...')
+    print("Counting mutations per gene...")
     mut_freq = count_mutations_per_gene_and_effect(snv, id_col, gene_col, effect_col)
-    print('Computing entropy per gene...')
-    mut_entropy = compute_mutation_entropy_per_gene_and_effect(snv, annot, gene_col, effect_col)
-    result = pd.merge(mut_freq, mut_entropy, how='left', on=[gene_col,effect_col])
-    result['mut_freq_per_kb'] = (result['n'] / result['length']) * 1000
-    
+    print("Computing entropy per gene...")
+    mut_entropy = compute_mutation_entropy_per_gene_and_effect(
+        snv, annot, gene_col, effect_col
+    )
+    result = pd.merge(mut_freq, mut_entropy, how="left", on=[gene_col, effect_col])
+    result["mut_freq_per_kb"] = (result["n"] / result["length"]) * 1000
+
     # save
-    result.to_csv(output_file, sep='\t', index=False, compression='gzip')
+    result.to_csv(output_file, sep="\t", index=False, compression="gzip")
 
 
 ##### SCRIPT #####
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-    print('Done!')
-
+    print("Done!")