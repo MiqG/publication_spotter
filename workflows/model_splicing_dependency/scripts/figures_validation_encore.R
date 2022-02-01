0bcf8f0e96fcd322520137d934f629ff41653ff9
MiqG <miquelangladagirotto@gmail.com>
2022-02-01 17:02:47 +0100

    added validation encore

--- workflows/model_splicing_dependency/scripts/figures_validation_encore.R
+++ workflows/model_splicing_dependency/scripts/figures_validation_encore.R
@@ -16,6 +16,7 @@ require(scattermore)
 require(ggrepel)
 require(extrafont)
 require(tidytext)
+require(clusterProfiler)
 
 ROOT = here::here()
 source(file.path(ROOT,"src","R","utils.R"))
@@ -30,10 +31,12 @@ THRESH_DPSI = 10
 # RESULTS_DIR = file.path(ROOT,"results","model_splicing_dependency")
 # rnai_file = file.path(PREP_DIR,"demeter2","CCLE.tsv.gz")
 # delta_psi_file = file.path(RESULTS_DIR,'files','ENCORE','delta_psi-EX.tsv.gz')
-# norm_spldep_file = file.path(RESULTS_DIR,"files","ENCORE","norm_spldep-EX.tsv.gz")
+# harm_score_file = file.path(RESULTS_DIR,"files","ENCORE","harm_score-EX.tsv.gz")
 # selected_events_file = file.path(RESULTS_DIR,"files","selected_models-EX.txt")
 # event_info_file = file.path(RAW_DIR,"VastDB","EVENT_INFO-hg38_noseqs.tsv")
 # metadata_file = file.path(PREP_DIR,'metadata','ENCORE.tsv.gz')
+# ontology_file = file.path(RESULTS_DIR,'files','ENCORE','kd_gene_sets-EX.tsv.gz')
+
 
 ##### FUNCTIONS #####
 plot_encore_validation = function(){
@@ -42,12 +45,14 @@ plot_encore_validation = function(){
     cells_oi = metadata %>% pull(DepMap_ID) %>% unique()
     genes_kd = metadata %>% pull(KD) %>% unique() %>% na.omit()
     event_annot = event_info %>% distinct(GENE, EVENT)
+    metadata = metadata %>% mutate(kd_lab=paste0(KD,"_",cell_line))
     
     genedep = rnai %>% 
         filter(index%in%genes_kd) %>% 
-        dplyr::select(c("index",cells_oi)) %>% 
+        dplyr::select(c("index",all_of(cells_oi))) %>% 
         pivot_longer(-index, names_to="DepMap_ID", values_to="demeter2") %>% 
         rename(KD=index) %>%
+        distinct() %>%
         drop_na()
     
     dpsi = delta_psi %>%
@@ -57,27 +62,97 @@ plot_encore_validation = function(){
         rename(index=EVENT) %>%
         drop_na()
     
-    X = norm_spldep %>% 
+    X = harm_score %>% 
         filter(index%in%selected_events) %>% 
-        pivot_longer(-index, names_to="sampleID", values_to="nspldep") %>%
-        filter(is.finite(nspldep)) %>%
+        pivot_longer(-index, names_to="sampleID", values_to="harm") %>%
+        filter(is.finite(harm) & harm>0) %>% # only positive harm score
         left_join(dpsi, by=c("index","sampleID")) %>%
         left_join(
-            metadata %>% mutate(lab=paste0(KD,"_",replicate)),
+            metadata %>% mutate(lab=paste0(KD,"_",replicate)) %>% distinct(sampleID,DepMap_ID,KD,cell_line),
             by="sampleID"
         ) %>%
         left_join(genedep, by=c("KD","DepMap_ID")) %>% 
-        mutate(sign_nspldep = sign(deltaPSI) * nspldep) %>% 
-        drop_na(deltaPSI)
+        drop_na(deltaPSI, demeter2) %>%
+        group_by(cell_line, KD, demeter2, index) %>%
+        summarize(deltaPSI = mean(deltaPSI),
+                  harm = mean(harm)) %>%
+        ungroup() %>%
+        mutate(sign_harm = sign(deltaPSI) * harm)
     
-    # the most harmful change
-    X %>% group_by(cell_line, KD) %>% slice_max(nspldep, n=1) %>% ggscatter(x="sign_nspldep",y="demeter2",color="KD",palette=get_palette("simpsons",length(genes_kd))) + guides(color="none") + stat_cor(method="spearman") +  geom_smooth(method="lm", linetype="dashed", color="black") + facet_wrap(~cell_line, ncol=1)
+    # how does correlation change summing different top maximum?
+    correls = lapply(1:25, function(x){
+        corr = X %>% 
+            group_by(cell_line, KD, demeter2) %>% 
+            slice_max(harm, n=x) %>% 
+            summarize(pred = sum(sign_harm),
+                      nobs = n()) %>% 
+            ungroup() %>% 
+            group_by(cell_line) %>% 
+            summarize(correlation = cor(demeter2, pred, method="pearson"), 
+                      pvalue=cor.test(demeter2, pred, method="pearson")[["p.value"]],
+                      log10_pvalue = -log10(pvalue),
+                      thresh = x)
+        return(corr)
+    })
+    correls = do.call(rbind, correls)
     
-    # a sum of changes
-    X %>% group_by(cell_line, KD, demeter2) %>% filter(nspldep>0) %>% summarize(pred=sum(sign_nspldep))  %>% ggscatter(x="pred",y="demeter2",color="KD",palette=get_palette("simpsons",length(genes_kd))) + guides(color="none") + stat_cor(method="spearman") +  geom_smooth(method="lm", linetype="dashed", color="black") + facet_wrap(~cell_line, ncol=1, scales="free")
+    plts[["encore_val-thresh_vs_pearsons"]] = correls %>% 
+        ggscatter(x="thresh", y="correlation", palette="Set2",
+                  size="log10_pvalue", color="cell_line") + 
+        labs(x="N Most Harmful Exons", y="Pearson Correlation", 
+             color="Cell Line", size="-log10(p-value)")
+    
+    # the most harmful changes predict overall knockdown
+    x = X %>% 
+        group_by(cell_line, KD, demeter2) %>% 
+        slice_max(harm, n=1) %>% 
+        summarize(pred=sum(sign_harm)) %>%
+        group_by(cell_line) %>%
+        mutate(cell_line_lab=sprintf("%s (n=%s)", cell_line, n()))
+    plts[["encore_val-top1-scatters"]] = x %>%
+        ggscatter(x="pred", y="demeter2", color="KD",
+                  palette=get_palette("simpsons",length(genes_kd))) + 
+        guides(color="none") + stat_cor(method="pearson") + 
+        geom_smooth(method="lm", linetype="dashed", color="black") + 
+        facet_wrap(~cell_line_lab, ncol=1, scales="free") + 
+        geom_text_repel(aes(label=KD),
+                        x %>% slice_max(demeter2*pred, n=5)) +
+        labs(x="Sum of Top Harm Scores", y="Gene Dependency")
+    
+    x = X %>% 
+        group_by(cell_line, KD, demeter2) %>% 
+        slice_max(harm, n=10) %>% 
+        summarize(pred=sum(sign_harm)) %>%
+        group_by(cell_line) %>%
+        mutate(cell_line_lab=sprintf("%s (n=%s)", cell_line, n()))
+    plts[["encore_val-top10-scatters"]] = x %>%
+        ggscatter(x="pred", y="demeter2", color="KD",
+                  palette=get_palette("simpsons",length(genes_kd))) + 
+        guides(color="none") + stat_cor(method="pearson") + 
+        geom_smooth(method="lm", linetype="dashed", color="black") + 
+        facet_wrap(~cell_line_lab, ncol=1, scales="free") + 
+        geom_text_repel(aes(label=KD),
+                        x %>% slice_max(demeter2*pred, n=5)) +
+        labs(x="Sum of Top Harm Scores", y="Gene Dependency")
     
     # correlation between cell lines gene dependencies
-    genedep %>% left_join(metadata %>% distinct(DepMap_ID,cell_line)) %>% pivot_wider(id_cols="KD", names_from="cell_line", values_from="demeter2") %>% ggscatter(x="K562", y="HepG2") + stat_cor(method="spearman") + geom_smooth(method="lm", linetype="dashed", color="black")
+    plts[["event_val-demeter2-scatter"]] = genedep %>% 
+        left_join(metadata %>% distinct(DepMap_ID,cell_line), by="DepMap_ID") %>% 
+        pivot_wider(id_cols="KD", 
+                    names_from="cell_line", 
+                    values_from="demeter2") %>% 
+        ggscatter(x="K562", y="HepG2") + 
+        stat_cor(method="pearson") + 
+        geom_smooth(method="lm", linetype="dashed", color="black")
+    
+    # are some events changing in KDs enriched in our selected events? No.
+    enrichment = enricher(
+        selected_events, TERM2GENE=ontology, 
+        universe=event_annot %>%  filter(str_detect(EVENT,"EX")) %>% 
+          pull(EVENT) %>% unique(), 
+        maxGSSize=Inf)
+    
+    plts[["event_val-enrichment-KD"]] = dotplot(enrichment) + theme_pubr()
     
     return(plts)
 }
@@ -158,9 +233,10 @@ main = function(){
     metadata = read_tsv(metadata_file)
     delta_psi = read_tsv(delta_psi_file)
     rnai = read_tsv(rnai_file)
-    norm_spldep = read_tsv(norm_spldep_file)
+    harm_score = read_tsv(harm_score_file)
     selected_events = readLines(selected_events_file)
     event_info = read_tsv(event_info_file)
+    ontology = read_tsv(ontology_file)
     
     plts = make_plots()
 
