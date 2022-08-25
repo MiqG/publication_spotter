f7bae64a21ffb24726823f7b331283c3af47f6a0
MiqG <miquelangladagirotto@gmail.com>
2022-08-25 18:58:29 +0200

    refactored Thomas2020 to include distances of mapping error

--- workflows/preprocess_data/scripts/preprocess_Thomas2020.R
+++ workflows/preprocess_data/scripts/preprocess_Thomas2020.R
@@ -10,31 +10,31 @@ require(readxl)
 require(tidyverse)
 
 ROOT = here::here()
-source(file.path(ROOT,'src','R','utils.R'))
+source(file.path(ROOT,"src","R","utils.R"))
 
 # Development
 # -----------
-# RAW_DIR = file.path(ROOT,'data','raw')
-# thomas_crispr_screen_file = file.path(RAW_DIR,'articles','Thomas2020','crispr_screen.xlsx')
-# thomas_event_mapping_file = file.path(RAW_DIR,'articles','Thomas2020','event_mapping_vastdb.tsv')
+# RAW_DIR = file.path(ROOT,"data","raw")
+# thomas_crispr_screen_file = file.path(RAW_DIR,"articles","Thomas2020","crispr_screen.xlsx")
+# thomas_event_mapping_file = file.path(RAW_DIR,"articles","Thomas2020","event_mapping_vastdb.tsv")
 
 
 ##### FUNCTIONS #####
 prep_thomas = function(X){
-    cols = grep('\\|', colnames(X), value = TRUE)
-    conditions = do.call(rbind,strsplit(cols, '\\|'))
+    cols = grep("\\|", colnames(X), value = TRUE)
+    conditions = do.call(rbind,strsplit(cols, "\\|"))
     stats = unique(conditions[,1])
     conditions = unique(conditions[,2])
     
     screen = lapply(conditions, function(cond){
         screen = X %>% dplyr::select(contains(cond))
         colnames(screen) = stats
-        cond = unlist(strsplit(cond, '\\.'))
+        cond = unlist(strsplit(cond, "\\."))
         screen = screen %>% 
             mutate(cell_line=cond[1], 
                    comparison=cond[2], 
                    replicate=cond[3])
-        cols_oi = c('event','EVENT','target_type','geneName')
+        cols_oi = c("event","EVENT","target_type","geneName","distance_mapping_error")
         screen = cbind(X[,cols_oi],screen)
         return(screen)
     })
@@ -51,12 +51,12 @@ load_crispr_screen = function(
     thomas_mapping = read_tsv(thomas_event_mapping_file)
 
     thomas_crispr_screen = thomas_crispr_screen %>% 
-        left_join(thomas_mapping, by=c('event','target_type')) %>%
+        left_join(thomas_mapping, by=c("event","target_type")) %>%
         filter(!is.na(EVENT))
     thomas_crispr_screen = prep_thomas(thomas_crispr_screen)
     thomas_crispr_screen = thomas_crispr_screen %>% mutate(
         fitness_score = log2(fc_norm_mean),
-        study = 'Thomas (2020)',
+        study = "Thomas (2020)",
         pvalue = pval,
         hit = fdr < 0.05
     )
