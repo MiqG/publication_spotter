22a52037f43543cdf1e5336fea713ce11dda7c14
MiqG <miquelangladagirotto@gmail.com>
2024-01-12 18:18:50 +0100

    added psi uncertainty analysis

---
+++ workflows/model_splicing_dependency/scripts/figures_psi_uncertainty.R
@@ -0,0 +1,308 @@
+#
+# Author: Miquel Anglada Girotto
+# Contact: miquel [dot] anglada [at] crg [dot] eu
+#
+# Script purpose
+# --------------
+#
+# Outline
+# -------
+# 1. Real dependencies of our selected exons in different conditions
+# 2. How do our predicted splicing dependencies compare to the real ones?
+
+require(optparse)
+require(tidyverse)
+require(ggrepel)
+require(ggpubr)
+require(cowplot)
+require(extrafont)
+require(scattermore)
+
+# variables
+RANDOM_SEED = 1234
+
+# formatting
+PAL_SINGLE_ACCENT = "orange"
+PAL_DUAL = "#0C0A3E"
+LINE_SIZE = 0.25
+
+FONT_SIZE = 2 # for additional labels
+FONT_FAMILY = "Arial"
+
+# Development
+# -----------
+# ROOT = here::here()
+# RAW_DIR = file.path(ROOT,"data","raw")
+# PREP_DIR = file.path(ROOT,"data","prep")
+# RESULTS_DIR = file.path(ROOT,"results","model_splicing_dependency")
+# event_info_file = file.path(RAW_DIR,"VastDB","EVENT_INFO-hg38_noseqs.tsv")
+# selected_events_file = file.path(RESULTS_DIR,"files","selected_models-EX.txt")
+# psi_file = file.path(PREP_DIR,"event_psi","inhouse-EX.tsv.gz")
+# total_reads_file = file.path(PREP_DIR,"event_total_reads","inhouse-EX.tsv.gz")
+# spldep_file = file.path(ROOT,"results","experimental_validation",'files','splicing_dependency-EX','mean.tsv.gz')
+# psi_simulated_file = file.path(RESULTS_DIR,"files","psi_uncertainty","psi_simulated","inhouse-EX.tsv.gz")
+# spldep_simulated_file = file.path(RESULTS_DIR,"files","psi_uncertainty",'splicing_dependency-EX','inhouse','mean.tsv.gz')
+# figs_dir = file.path(RESULTS_DIR,'figures','psi_uncertainty')
+
+##### FUNCTIONS #####
+simulate_psi_vs_read_count = function(n_reads_size, n_simulation=1000){
+    set.seed(RANDOM_SEED)
+    
+    psis = seq(0,1,0.05)
+    simulations = lapply(psis, function(psi){
+        simulation = rbinom(n=n_simulation, size=n_reads_size, prob=psi) / n_reads_size
+        simulation = simulation * 100 # rescale
+        simulation_summary = data.frame(
+            psi_prob = psi*100, # rescale
+            n_reads = n_reads_size,
+            simulation_mean = mean(simulation),
+            simulation_median = median(simulation),
+            simulation_std = sd(simulation),
+            simulation_q25 = quantile(simulation, probs=0.25),
+            simulation_q75 = quantile(simulation, probs=0.75)
+        ) %>%
+        mutate(simulation_iqr = simulation_q75 - simulation_q25)
+    }) %>% bind_rows()
+    
+    return(simulations)
+}
+
+
+plot_simulations = function(simulations){
+    plts = list()
+    
+    X = simulations
+    
+    plts[["simulations-psi_vs_std_vs_n_reads-line"]] = X %>%
+        mutate(n_reads = as.factor(n_reads)) %>%
+        ggline(x="psi_prob", y="simulation_std", color="n_reads", numeric.x.axis=TRUE) +
+        labs(x="PSI", y="Simulated PSI Std", color="N Reads to Compute PSI")
+    
+    return(plts)
+}
+
+
+plot_uncertainty_quant = function(uncertainty_quant){
+    plts = list()
+    
+    X = uncertainty_quant
+    
+    plts[["uncertainty_quant-distr_total_reads-hist"]] = X %>%
+        mutate(total_reads = log10(total_reads+1)) %>%
+        ggviolin(x="sampleID", y="total_reads", fill=PAL_SINGLE_ACCENT, color=NA, trim=TRUE) + 
+        geom_boxplot(fill=NA, outlier.size=0.1, width=0.2) +
+        labs(x="Sample", y="log10(Total Exon Reads+1)") +
+        theme_pubr(x.text.angle=70)
+    
+    plts[["uncertainty_quant-psi_real_vs_simulated-scatter"]] = X %>%
+        ggplot(aes(x=psi_real, y=psi_simulated)) +
+        geom_scattermore(pixels = c(1000,1000), pointsize=5, alpha=0.5, color="black") +
+        stat_cor(method="pearson", size=FONT_SIZE, family=FONT_FAMILY) +
+        theme_pubr() +
+        facet_wrap(~sampleID) +
+        theme(aspect.ratio=1, strip.text.x = element_text(size=6, family=FONT_FAMILY)) +
+        labs(x="PSI Real", y="PSI Simulated")
+    
+    plts[["uncertainty_quant-total_reads_vs_diff_psi-scatter"]] = X %>%
+        ggplot(aes(x=total_reads, y=abs(diff_psi))) +
+        geom_scattermore(pixels = c(1000,1000), pointsize=5, alpha=0.5, color="black") +
+        stat_cor(method="pearson", size=FONT_SIZE, family=FONT_FAMILY) +
+        geom_vline(xintercept=10, size=LINE_SIZE, color="black", linetype="dashed") +
+        theme_pubr() +
+        facet_wrap(~sampleID) +
+        theme(aspect.ratio=1, strip.text.x = element_text(size=6, family=FONT_FAMILY)) +
+        labs(x="Total Exon Reads", y="|PSI Real - PSI Simulated|")
+    
+    
+    plts[["uncertainty_quant-spldep_real_vs_simulated-scatter"]] = X %>%
+        ggplot(aes(x=spldep_real, y=spldep_simulated)) +
+        geom_scattermore(pixels = c(1000,1000), pointsize=5, alpha=0.5, color="black") +
+        stat_cor(method="pearson", size=FONT_SIZE, family=FONT_FAMILY) +
+        theme_pubr() +
+        facet_wrap(~sampleID) +
+        theme(aspect.ratio=1, strip.text.x = element_text(size=6, family=FONT_FAMILY)) +
+        labs(x="Spl. Dep. Real", y="Spl. Dep. Simulated")
+    
+    
+    plts[["uncertainty_quant-total_reads_vs_diff_spldep-scatter"]] = X %>%
+        ggplot(aes(x=total_reads, y=abs(diff_spldep))) +
+        geom_scattermore(pixels = c(1000,1000), pointsize=5, alpha=0.5, color="black") +
+        stat_cor(method="pearson", size=FONT_SIZE, family=FONT_FAMILY) +
+        geom_vline(xintercept=10, size=LINE_SIZE, color="black", linetype="dashed") +
+        theme_pubr() +
+        facet_wrap(~sampleID) +
+        theme(aspect.ratio=1, strip.text.x = element_text(size=6, family=FONT_FAMILY)) +
+        labs(x="Total Exon Reads", y="|Spl. Dep. Real - Spl. Dep. Simulated|")
+    
+    
+    plts[["uncertainty_quant-diff_psi_vs_diff_spldep-scatter"]] = X %>%
+        ggplot(aes(x=abs(diff_psi), y=abs(diff_spldep))) +
+        geom_scattermore(pixels = c(1000,1000), pointsize=5, alpha=0.5, color="black") +
+        stat_cor(method="pearson", size=FONT_SIZE, family=FONT_FAMILY) +
+        theme_pubr() +
+        facet_wrap(~sampleID) +
+        theme(aspect.ratio=1, strip.text.x = element_text(size=6, family=FONT_FAMILY)) +
+        labs(x="|PSI Real - PSI Simulated|", y="|Spl. Dep. Real - Spl. Dep. Simulated|")
+    
+    
+    return(plts)
+}
+
+
+make_plots = function(simulations, uncertainty_quant){
+    plts = list(
+        plot_simulations(simulations),
+        plot_uncertainty_quant(uncertainty_quant)
+    )
+    plts = do.call(c,plts)
+    return(plts)
+}
+
+
+make_figdata = function(simulations, uncertainty_quant){
+    figdata = list(
+        "psi_uncertainty" = list(
+            "simulations" = simulations,
+            "uncertainty_quant" = uncertainty_quant
+        )
+    )
+    return(figdata)
+}
+
+
+save_plt = function(plts, plt_name, extension=".pdf", 
+                    directory="", dpi=350, format=TRUE,
+                    width = par("din")[1], height = par("din")[2]){
+    print(plt_name)
+    plt = plts[[plt_name]]
+    if (format){
+        plt = ggpar(plt, font.title=8, font.subtitle=8, font.caption=8, 
+                    font.x=8, font.y=8, font.legend=6,
+                    font.tickslab=6, font.family=FONT_FAMILY, device=cairo_pdf)    
+    }
+    filename = file.path(directory,paste0(plt_name,extension))
+    save_plot(filename, plt, base_width=width, base_height=height, dpi=dpi, units="cm")
+}
+
+
+save_plots = function(plts, figs_dir){
+    save_plt(plts, "simulations-psi_vs_std_vs_n_reads-line", ".pdf", figs_dir, width=5, height=5.5)
+    
+    save_plt(plts, "simulations-psi_vs_std_vs_n_reads-line", ".pdf", figs_dir, width=5, height=5.5)
+    save_plt(plts, "simulations-psi_vs_std_vs_n_reads-line", ".pdf", figs_dir, width=5, height=5.5)
+    save_plt(plts, "simulations-psi_vs_std_vs_n_reads-line", ".pdf", figs_dir, width=5, height=5.5)
+    save_plt(plts, "simulations-psi_vs_std_vs_n_reads-line", ".pdf", figs_dir, width=5, height=5.5)
+    save_plt(plts, "simulations-psi_vs_std_vs_n_reads-line", ".pdf", figs_dir, width=5, height=5.5)
+    save_plt(plts, "simulations-psi_vs_std_vs_n_reads-line", ".pdf", figs_dir, width=5, height=5.5)
+    save_plt(plts, "simulations-psi_vs_std_vs_n_reads-line", ".pdf", figs_dir, width=5, height=5.5)
+}
+
+
+save_figdata = function(figdata, dir){
+    lapply(names(figdata), function(x){
+        d = file.path(dir,'figdata',x)
+        dir.create(d, recursive=TRUE)
+        lapply(names(figdata[[x]]), function(nm){
+            df = figdata[[x]][[nm]]
+            filename = file.path(d, paste0(nm,'.tsv.gz'))
+            write_tsv(df, filename)
+            
+            print(filename)
+        })
+    })
+}
+
+parseargs = function(){
+    
+    option_list = list( 
+        make_option("--psi_file", type="character"),
+        make_option("--total_reads_file", type="character"),
+        make_option("--psi_simulated_file", type="character"),
+        make_option("--spldep_file", type="character"),
+        make_option("--spldep_simulated_file", type="character"),
+        make_option("--selected_events_file", type="character"),
+        make_option("--figs_dir", type="character")
+    )
+
+    args = parse_args(OptionParser(option_list=option_list))
+    
+    return(args)
+}
+
+main = function(){
+    args = parseargs()
+    
+    psi_file = args[["psi_file"]]
+    total_reads_file = args[["total_reads_file"]]
+    psi_simulated_file = args[["psi_simulated_file"]]
+    spldep_file = args[["spldep_file"]]
+    spldep_simulated_file = args[["spldep_simulated_file"]]
+    selected_events_file = args[["selected_events_file"]]
+    figs_dir = args[["figs_dir"]]
+    
+    dir.create(figs_dir, recursive = TRUE)
+    
+    # load
+    selected_events = readLines(selected_events_file)
+    psi = read_tsv(psi_file)
+    total_reads = read_tsv(total_reads_file)
+    spldep = read_tsv(spldep_file)
+    psi_simulated = read_tsv(psi_simulated_file)
+    spldep_simulated = read_tsv(spldep_simulated_file)
+    
+    # preprocess
+    uncertainty_quant = psi %>%
+        filter(EVENT %in% selected_events) %>%
+        pivot_longer(-EVENT, names_to="sampleID", values_to="psi_real") %>%
+        left_join(
+            total_reads %>%
+            filter(EVENT %in% selected_events) %>%
+            pivot_longer(-EVENT, names_to="sampleID", values_to="total_reads"),
+            by=c("EVENT","sampleID")
+        ) %>%
+        left_join(
+            spldep %>%
+            dplyr::rename(EVENT=index) %>%
+            filter(EVENT %in% selected_events) %>%
+            pivot_longer(-EVENT, names_to="sampleID", values_to="spldep_real"),
+            by=c("EVENT","sampleID")
+        ) %>%
+        left_join(
+            psi_simulated %>%
+            filter(EVENT %in% selected_events) %>%
+            pivot_longer(-EVENT, names_to="sampleID", values_to="psi_simulated"),
+            by=c("EVENT","sampleID")
+        ) %>%
+        left_join(
+            spldep_simulated %>%
+            dplyr::rename(EVENT=index) %>%
+            filter(EVENT %in% selected_events) %>%
+            pivot_longer(-EVENT, names_to="sampleID", values_to="spldep_simulated"),
+            by=c("EVENT","sampleID")
+        ) %>%
+        mutate(
+            diff_psi = psi_real - psi_simulated,
+            diff_spldep = spldep_real - spldep_simulated
+        )
+    
+    # simulate
+    n_reads = c(1,10,100,1000)
+    simulations = lapply(n_reads, simulate_psi_vs_read_count) %>% bind_rows()
+    
+    # plot
+    plts = make_plots(simulations, uncertainty_quant)
+
+    # make figdata
+    figdata = make_figdata(simulations, uncertainty_quant)
+    
+    # save
+    save_plots(plts, figs_dir)
+    save_figdata(figdata, figs_dir)
+}
+
+
+##### SCRIPT #####
+if (sys.nframe() == 0L) {
+    main()
+    print("Done!")
+}
--- workflows/obtain_data/scripts/get_raw_reads.py
+++ workflows/obtain_data/scripts/get_raw_reads.py
@@ -20,8 +20,9 @@ import os
 ROOT = '/home/miquel/projects/publication_spotter'
 RAW_DIR = os.path.join(ROOT,'data','raw')
 inclusion_levels_file = os.path.join(RAW_DIR,"TCGA","GBM","vast_out","INCLUSION_LEVELS_FULL-hg38-175.tab.gz")
+inclusion_levels_file = os.path.join(RAW_DIR,"articles",'Thomas2020','vast_out','INCLUSION_LEVELS_FULL-hg38-2.tab.gz')
 n_jobs = 10
-chunksize = 10
+chunksize = 1000
 """
 
 ##### FUNCTIONS #####
@@ -29,25 +30,55 @@ def get_total_rows(filename):
     return len(pd.read_table(filename, usecols=[0]))
     
 def get_inclusion_reads(x):
-    # score3 contains INC1=INC2=EXC (reads mapping inclusion and exclusion junctions)
-    score3 = x.str.split(",").str[2]
-    inclusion_reads = score3.str.split("=", expand=True)[[0,1]].astype("int64").sum(axis=1)
+    # score3 contains INC1=INC2=EXC or INC=EXC (reads mapping inclusion and exclusion junctions)
+    ## some scores are not in the correct order
+    scores = x.str.replace(",=",",").str.split(",", expand=True)
+    score3 = scores[2]
+    is_alt_or_int = scores.index.str.contains("ALT|INT")
+    if any(is_alt_or_int):
+        score3[is_alt_or_int] = scores.loc[is_alt_or_int,3]
+        
+    n_ss = score3.str.count("=")
+    idx_two_ss = n_ss==2
+    idx_one_ss = n_ss==1
+    
+    inclusion_reads = score3.copy()
+    if any(idx_two_ss):
+        inclusion_reads.loc[idx_two_ss] = inclusion_reads.loc[idx_two_ss].str.split("=", expand=True)[[0,1]].replace("",None).astype("int64").sum(axis=1)
+    if any(idx_one_ss):        
+        inclusion_reads.loc[idx_one_ss] = inclusion_reads.loc[idx_one_ss].str.split("=", expand=True)[0].replace("",None).astype("int64")
+    
     return inclusion_reads
 
 
 def get_exclusion_reads(x):
-    # score3 contains INC1=INC2=EXC (reads mapping inclusion and exclusion junctions)
-    score3 = x.str.split(",").str[2]
-    exclusion_reads = score3.str.split("=", expand=True)[2].astype("int64")
+    # score3 contains INC1=INC2=EXC or INC=EXC (reads mapping inclusion and exclusion junctions)
+    ## some scores are not in the correct order
+    scores = x.str.replace(",=",",").str.split(",", expand=True)
+    score3 = scores[2]
+    is_alt_or_int = scores.index.str.contains("ALT|INT")
+    if any(is_alt_or_int):
+        score3[is_alt_or_int] = scores.loc[is_alt_or_int,3]
+        
+    n_ss = score3.str.count("=")
+    idx_two_ss = n_ss==2
+    idx_one_ss = n_ss==1
+    
+    exclusion_reads = score3.copy()
+    if any(idx_two_ss):
+        exclusion_reads.loc[idx_two_ss] = exclusion_reads.loc[idx_two_ss].str.split("=", expand=True)[2].replace("",None).astype("int64")
+    if any(idx_one_ss):        
+        exclusion_reads.loc[idx_one_ss] = exclusion_reads.loc[idx_one_ss].str.split("=", expand=True)[1].replace("",None).astype("int64")
+    
     return exclusion_reads
 
     
 def get_raw_reads(inclusion_levels):
-    
+
     # subset event and quality columns
-    cols_oi = ["EVENT"] + [c for c in inclusion_levels.columns if "_1-Q" in c]
+    cols_oi = ["EVENT"] + [c for c in inclusion_levels.columns if c.endswith("-Q")]
     inclusion_levels = inclusion_levels[cols_oi].set_index("EVENT")
-    
+
     # get read counts
     inclusion_reads = inclusion_levels.apply(get_inclusion_reads, axis=0)
     exclusion_reads = inclusion_levels.apply(get_exclusion_reads, axis=0)
@@ -60,6 +91,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--inclusion_levels_file", type=str)
     parser.add_argument("--n_jobs", type=int, default=N_JOBS)
+    parser.add_argument("--chunksize", type=int, default=1000)
     parser.add_argument("--inclusion_reads_file", type=str)
     parser.add_argument("--exclusion_reads_file", type=str)
     parser.add_argument("--total_reads_file", type=str)
@@ -83,11 +115,13 @@ def main():
     # load
     print("Loading data...")
     inclusion_levels_chunks = pd.read_table(inclusion_levels_file, chunksize=chunksize)
-
+    
     # preprocess
     print("Preprocessing...")
     results = Parallel(n_jobs=n_jobs)(
-        delayed(get_raw_reads)(inclusion_levels_chunk) 
+        delayed(get_raw_reads)(
+            inclusion_levels_chunk
+        )
         for inclusion_levels_chunk in tqdm(inclusion_levels_chunks)
     )
     
@@ -108,9 +142,9 @@ def main():
     
     # save
     print("Saving data...")
-    inclusion_reads.to_csv(inclusion_reads_file, **SAVE_PARAMS)
-    exclusion_reads.to_csv(exclusion_reads_file, **SAVE_PARAMS)
-    total_reads.to_csv(total_reads_file, **SAVE_PARAMS)
+    inclusion_reads.reset_index().to_csv(inclusion_reads_file, **SAVE_PARAMS)
+    exclusion_reads.reset_index().to_csv(exclusion_reads_file, **SAVE_PARAMS)
+    total_reads.reset_index().to_csv(total_reads_file, **SAVE_PARAMS)
 
 
 ##### SCRIPT #####
